cd9b0783affce511f43f603becbddf5262756f82
encrypticus <encrypticus@gmail.com>
2020-07-25 13:20:58 +0600

    Добавил пакет babel-plugin-styled-components.

--- webpack/configs/babel.config.js
+++ webpack/configs/babel.config.js
@@ -13,7 +13,8 @@ module.exports = (api) => {
   ];
 
   const plugins = [
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    'babel-plugin-styled-components'
   ];
 
   return {
