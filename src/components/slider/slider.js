211a3b72f20cf09e01a3453f95961adc4bdfc810
encrypticus <encrypticus@gmail.com>
2020-08-04 19:16:57 +0600

    Добавил компонент Slider.

---
+++ src/components/slider/slider.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef } from 'react';
+import './slider.scss';
+import noUiSlider from 'nouislider';
+import 'nouislider/distribute/nouislider.min.css';
+
+const Slider = (props) => {
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const slider = sliderRef.current;
+
+    noUiSlider.create(slider, {
+      start: [20, 80],
+      connect: true,
+      range: {
+        'min': 0,
+        'max': 100
+      }
+    });
+
+    slider.noUiSlider.on('change', function (values) {
+      console.log(values);
+    });
+  }, []);
+
+  return (
+    <div className='slider' ref={sliderRef}></div>
+  );
+};
+
+export default Slider;
--- src/pages/catalog/catalogPage.js
+++ src/pages/catalog/catalogPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '#comps/header';
 import Cards from '../../blocks/cards';
+import Slider from '#comps/slider';
 
 const CatalogPage = (props) => {
 
   return (
     <>
       <Header menuHidden/>
+      <Slider/>
       <Cards/>
     </>
   );
