83b843876a94deb55bac1ceec0fef128df75f47e
encrypticus <encrypticus@gmail.com>
2020-08-04 17:09:55 +0600

    Дополнил страницу каталога.

--- src/pages/catalog/catalogPage.js
+++ src/pages/catalog/catalogPage.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from '#comps/header';
-import HeaderNav from '#comps/header-nav';
-import Card from '#comps/card';
+import Cards from '../../blocks/cards';
 
 const CatalogPage = (props) => {
 
   return (
     <>
       <Header menuHidden/>
-      <Card vendor='Techine' price='12 700' src='./img/watch_1.png'/>
+      <Cards/>
     </>
   );
 };
