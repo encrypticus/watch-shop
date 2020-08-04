b91fdaba614ce1c3626edc4eaab7c9c6de7bdad9
encrypticus <encrypticus@gmail.com>
2020-08-04 22:20:23 +0600

    Добавил блок filter-form.

---
+++ src/blocks/filter-form/filter-form.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './filter-form.scss';
+import Filter from '#comps/filter';
+import { FilterItem } from '#comps/filter/filter';
+import Slider from '#comps/slider';
+import Checkbox from '#comps/checkbox';
+
+const FilterForm = (props) => {
+  return (
+    <form className='filter__form'>
+      <Filter>
+        <FilterItem title='Стоимость'>
+          <Slider/>
+        </FilterItem>
+      </Filter>
+
+      <Filter>
+        <FilterItem title='Бренд'>
+          <Checkbox id='techne'>
+            Techne
+          </Checkbox>
+          <Checkbox id='rado'>
+            Rado
+          </Checkbox>
+          <Checkbox id='bvlgari'>
+            Bvlgari
+          </Checkbox>
+          <Checkbox id='tissot'>
+            Tissot
+          </Checkbox>
+          <Checkbox id='omega'>
+            Omega
+          </Checkbox>
+          <Checkbox id='montblanc'>
+            Montblanc
+          </Checkbox>
+        </FilterItem>
+
+        <FilterItem title='Механизм'>
+          <Checkbox id='quartz'>
+            Кварц
+          </Checkbox>
+          <Checkbox id='mechanic'>
+            Механика
+          </Checkbox>
+        </FilterItem>
+
+        <FilterItem title='Материал'>
+          <Checkbox id='plastic'>
+            Пластик
+          </Checkbox>
+
+          <Checkbox id='still'>
+            Металл
+          </Checkbox>
+        </FilterItem>
+
+        <FilterItem title='Цвет'>
+          <Checkbox id='black'>
+            Черный
+          </Checkbox>
+          <Checkbox id='brown'>
+            Коричневый
+          </Checkbox>
+          <Checkbox id='yellow'>
+            Желтый
+          </Checkbox>
+          <Checkbox id='red'>
+            Красный
+          </Checkbox>
+        </FilterItem>
+      </Filter>
+    </form>
+  );
+};
+
+export default FilterForm;
--- src/pages/catalog/catalogPage.js
+++ src/pages/catalog/catalogPage.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Header from '#comps/header';
 import Cards from '../../blocks/cards';
 import Slider from '#comps/slider';
+import Checkbox from '#comps/checkbox';
+import Filter from '#comps/filter';
+import { FilterItem } from '#comps/filter/filter';
+import FilterForm from '../../blocks/filter-form';
 
 const CatalogPage = (props) => {
 
   return (
     <>
       <Header menuHidden/>
-      <Slider/>
+
       <Cards/>
+
+      <FilterForm/>
+
     </>
   );
 };
