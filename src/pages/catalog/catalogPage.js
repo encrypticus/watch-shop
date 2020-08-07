a0825816ceebf21611b0d50e7b78e2842c59b9d1
encrypticus <encrypticus@gmail.com>
2020-08-07 19:46:00 +0600

    Обновил страницу каталога.

--- src/pages/catalog/catalogPage.js
+++ src/pages/catalog/catalogPage.js
@@ -4,6 +4,8 @@ import Cards from '../../blocks/cards';
 import FilterForm from '../../blocks/filter-form';
 import './catalog-page.scss';
 import Subscribe from '#comps/subscribe';
+import BandCard from '#comps/band-card';
+import BandCarousel from '#comps/band-carousel';
 
 const CatalogPage = (props) => {
 
@@ -21,6 +23,20 @@ const CatalogPage = (props) => {
 
       </div>
       <Subscribe/>
+      <BandCarousel>
+        <BandCard vendor='Georg Jensen' price='6 500 ₽' src='./img/band1.png'/>
+        <BandCard vendor='Nato' price='3 200 ₽' src='./img/band2.png'/>
+        <BandCard vendor='Spark' price='5 400 ₽' src='./img/band3.png'/>
+        <BandCard vendor='Georg Jensen' price='6 500 ₽' src='./img/band1.png'/>
+        <BandCard vendor='Nato' price='3 200 ₽' src='./img/band2.png'/>
+        <BandCard vendor='Spark' price='5 400 ₽' src='./img/band3.png'/>
+        <BandCard vendor='Georg Jensen' price='6 500 ₽' src='./img/band1.png'/>
+        <BandCard vendor='Nato' price='3 200 ₽' src='./img/band2.png'/>
+        <BandCard vendor='Spark' price='5 400 ₽' src='./img/band3.png'/>
+        <BandCard vendor='Georg Jensen' price='6 500 ₽' src='./img/band1.png'/>
+        <BandCard vendor='Nato' price='3 200 ₽' src='./img/band2.png'/>
+        <BandCard vendor='Spark' price='5 400 ₽' src='./img/band3.png'/>
+      </BandCarousel>
     </>
   );
 };
