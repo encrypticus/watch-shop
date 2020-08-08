4674d353c6678332809a46afe3b429b442e12d03
encrypticus <encrypticus@gmail.com>
2020-08-08 10:07:16 +0600

    Добавил компонент Breadcrumbs.

---
+++ src/components/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './breadcrumbs.scss';
+import { NavLink } from 'react-router-dom';
+
+const Breadcrumbs = (props) => {
+  return (
+    <div className='breadcrumbs'>
+      <NavLink className='breadcrumbs__link' activeClassName='breadcrumbs__link_active' to='/' exact='true'>
+        Главная
+      </NavLink>
+      <NavLink className='breadcrumbs__link' activeClassName='breadcrumbs__link_active' to='./catalog'>
+        Каталог
+      </NavLink>
+      <NavLink className='breadcrumbs__link' activeClassName='breadcrumbs__link_active' to='./catalog'>
+        Мужские часы
+      </NavLink>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
