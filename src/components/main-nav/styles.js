42c6f9b66d20d77c8a0a25c6e96ab5599549666d
encrypticus <encrypticus@gmail.com>
2020-08-12 19:19:00 +0600

    Component MainNav: рефакторинг.

--- src/components/main-nav/styles.js
+++ src/components/main-nav/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Nav = styled.div`
+  box-sizing: border-box;
   position: fixed;
   top: 0;
   bottom: 0;
@@ -10,13 +11,20 @@ export const Nav = styled.div`
   min-height: 100vh;
   height: 100%;
   padding: 10px;
-  background-color: #444240;
+  //background-color: #444240;
+  background-color: rgba(0, 0, 0, .9);
   transform: ${(props) => (props.opened ? 'translateX(0)' : 'translateX(-30vw)')};
   z-index: 10;
   transition: transform .5s;
+  
   @media(max-width: 960px) {
     min-width: 50vw;
     transform: ${(props) => (props.opened ? 'translateX(0)' : 'translateX(-50vw)')};
+  }
+  
+  @media(max-width: 480px) {
+    min-width: 100vw;
+    transform: ${(props) => (props.opened ? 'translateX(0)' : 'translateX(-100vw)')};
   }
 `;
 
