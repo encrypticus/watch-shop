0deb35429aac0a14ba47223624c2dab9cb95260e
encrypticus <encrypticus@gmail.com>
2020-08-27 10:44:29 +0600

    Добавил логику авторизации в redux-store.

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,21 @@
+export const FETCH_AUTH_REQUEST = 'FETCH_AUTH_REQUEST';
+export const AUTH_REQUEST_FETCHING = 'AUTH_REQUEST_FETCHING';
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_UP = 'SIGN_UP';
+export const SIGN_OUT = 'SIGN_OUT';
+export const HAS_AUTH_ERROR = 'HAS_AUTH_ERROR';
+
+export const fetchAuth = (userData) => ({
+  type: FETCH_AUTH_REQUEST,
+  payload: userData,
+});
+
+export const authRequestFetching = (fetching) => ({
+  type: AUTH_REQUEST_FETCHING,
+  payload: fetching,
+});
+
+export const hasAuthError = (error) => ({
+  type: HAS_AUTH_ERROR,
+  payload: error,
+});
---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,57 @@
+import {
+  SIGN_IN, SIGN_OUT, SIGN_UP, AUTH_REQUEST_FETCHING, HAS_AUTH_ERROR,
+} from '#act/auth';
+
+const initialState = {
+  isUserSignedIn: false,
+  isUserRegistered: false,
+  isFetching: false,
+  error: {
+    status: false,
+    message: '',
+  },
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return {
+        ...state,
+        isUserSignedIn: true,
+      };
+
+    case SIGN_OUT: {
+      return {
+        ...state,
+        isUserSignedIn: false,
+      };
+    }
+
+    case SIGN_UP: {
+      return {
+        ...state,
+        isUserRegistered: true,
+      };
+    }
+
+    case AUTH_REQUEST_FETCHING:
+      return {
+        ...state,
+        isFetching: action.payload,
+      };
+
+    case HAS_AUTH_ERROR:
+      return {
+        ...state,
+        error: {
+          status: action.payload.status,
+          message: action.payload.message,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
