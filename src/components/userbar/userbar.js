76c902a17241214fa71ad83b8001cc0ae983b802
encrypticus <encrypticus@gmail.com>
2020-08-29 20:58:21 +0600

    Component Userbar: рефакторинг.

--- src/components/userbar/userbar.js
+++ src/components/userbar/userbar.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Wobble from 'react-reveal/Wobble';
 import './userbar.scss';
 import HeartIcon from './heart-icon';
 import CartIcon from './cart-icon';
 import User from '#comps/user';
 
-const Userbar = () => (
-  <div className='userbar'>
-    <User/>
+const Userbar = () => {
+  const { isUserSignedIn } = useSelector((state) => state.authReducer);
+  const { animateUserBar } = useSelector((state) => state.animationReducer);
 
-    <a href='/'>
-      <HeartIcon/>
-    </a>
+  const carts = (
+    <Wobble when={animateUserBar}>
+      <Link to='/bookmarks' title='перейти в избранное' className='userbar__link'>
+        <HeartIcon/>
+      </Link>
 
-    <a href='/'>
-      <CartIcon/>
-    </a>
-  </div>
-);
+      <Link to='/product-cart' title='перейти в корзину' className='userbar__link'>
+        <CartIcon/>
+      </Link>
+    </Wobble>
+  );
+
+  return (
+    <div className='userbar'>
+      <User/>
+      {isUserSignedIn && carts}
+    </div>
+  );
+};
 
 export default Userbar;
