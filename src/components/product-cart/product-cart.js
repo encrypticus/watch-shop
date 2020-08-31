7a8226793a8e2aad6c7d506b265a65b8a2048b47
encrypticus <encrypticus@gmail.com>
2020-08-31 15:02:29 +0600

    Component ProductCart: добавил начальную релаизацию получения корзины товаров с сервера.

--- src/components/product-cart/product-cart.js
+++ src/components/product-cart/product-cart.js
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import './product-cart.scss';
 import WatchesServiceProvider from '#context/context';
 
 const ProductCart = () => {
   const watchesService = useContext(WatchesServiceProvider);
+
+  useEffect(() => {
+    try {
+      watchesService.getProductCartFromDB()
+        .then((products) => console.log(products));
+    } catch (e) {
+      alert(e.message);
+    }
+  }, []);
 
   return (
     <div className='product-cart'>ProductCart</div>
