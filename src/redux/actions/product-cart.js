2298a55d741ef8027e1d68396b685d43aa2af2c4
encrypticus <encrypticus@gmail.com>
2020-09-01 20:43:57 +0600

    Redux store: добавил редьюсер и экшены для корзины товаров.

---
+++ src/redux/actions/product-cart.js
@@ -0,0 +1,24 @@
+export const FETCH_PRODUCT_CART_REQUEST = 'PRODUCT_CART_FETCHING_REQUEST';
+export const PRODUCT_CART_REQUEST_FETCHING = 'PRODUCT_CART_REQUEST_FETCHING';
+export const HAS_PRODUCT_CART_FETCHING_ERROR = 'HAS_PRODUCT_CART_FETCHING_ERROR';
+export const FILL_PRODUCT_CART = 'FILL_PRODUCT_CART';
+
+export const fetchProductCart = (products) => ({
+  type: FETCH_PRODUCT_CART_REQUEST,
+  payload: products
+});
+
+export const productCartRequestFetching = (fetching) => ({
+  type: PRODUCT_CART_REQUEST_FETCHING,
+  payload: fetching
+});
+
+export const hasProductCartFetchingError = (error) => ({
+  type: HAS_PRODUCT_CART_FETCHING_ERROR,
+  payload: error
+});
+
+export const fillProductCart = (products) => ({
+  type: FILL_PRODUCT_CART,
+  payload: products
+});
---
+++ src/redux/reducers/product-cart.js
@@ -0,0 +1,14 @@
+const initialState = {
+  products: [],
+  isFetching: false,
+  error: {
+    status: false,
+    message: ''
+  }
+};
+
+const productCartReducer = (state = initialState, action) => {
+
+};
+
+export default productCartReducer;
