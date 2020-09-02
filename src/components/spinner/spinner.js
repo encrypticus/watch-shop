9dfe7b1f789ef61c73c30bf5ff1e29f2b3c2d1c7
encrypticus <encrypticus@gmail.com>
2020-09-02 18:24:14 +0600

    В библиотеку компонентов добавил компонент Spinner.

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.scss';
+
+const Spinner = () => (
+  <div className='spinner'>
+    <div className='loadingio-spinner-double-ring-u5vw1hfnd2'>
+      <div className='ldio-6s7yrquvx2m'>
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Spinner;
