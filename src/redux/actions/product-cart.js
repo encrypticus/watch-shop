3b502f60d20f6ec2b95bf5340f2405e179dcfb1f
encrypticus <encrypticus@gmail.com>
2020-09-04 09:32:12 +0600

    Product cart actions: добавил новые экшены.

--- src/redux/actions/product-cart.js
+++ src/redux/actions/product-cart.js
@@ -1,5 +1,8 @@
 export const FETCH_PRODUCT_CART_REQUEST = 'PRODUCT_CART_FETCHING_REQUEST';
 export const PRODUCT_CART_REQUEST_FETCHING = 'PRODUCT_CART_REQUEST_FETCHING';
+export const ADD_PRODUCT_TO_CART_REQUEST = 'ADD_PRODUCT_TO_CART_REQUEST';
+export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
+export const ADD_PRODUCT_CART_REQUEST_FETCHING = 'ADD_PRODUCT_CART_REQUEST_FETCHING';
 export const HAS_PRODUCT_CART_FETCHING_ERROR = 'HAS_PRODUCT_CART_FETCHING_ERROR';
 export const FILL_PRODUCT_CART = 'FILL_PRODUCT_CART';
 
@@ -9,6 +12,21 @@ export const fetchProductCart = () => ({
 
 export const productCartRequestFetching = (fetching) => ({
   type: PRODUCT_CART_REQUEST_FETCHING,
+  payload: fetching,
+});
+
+export const addProductToCart = (product) => ({
+  type: ADD_PRODUCT_TO_CART,
+  payload: product,
+});
+
+export const addProductToCartRequest = (product) => ({
+  type: ADD_PRODUCT_TO_CART_REQUEST,
+  payload: product,
+});
+
+export const addProductToCartRequestFetching = (fetching) => ({
+  type: ADD_PRODUCT_CART_REQUEST_FETCHING,
   payload: fetching,
 });
 
