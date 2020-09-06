5283c2342754c362066978c2979461ed44323cf2
encrypticus <encrypticus@gmail.com>
2020-09-06 13:36:52 +0600

    Удаление/добавление товара в корзину. Привязка каталога товаров к информации о товарах в корзине.
    Добавил сагу для католога товаров.

---
+++ src/redux/sagas/product-catalog.js
@@ -0,0 +1,29 @@
+import {
+  take, put, call, all,
+} from 'redux-saga/effects';
+import watchesService from '#services/watches-service';
+import * as catalogActions from '#act/catalog-cards';
+
+function* fetchCatalog() {
+  while (true) {
+    try {
+      yield put(catalogActions.hasProductCatalogFetchingError({ status: false, message: '' }));
+
+      yield take(catalogActions.GET_PRODUCT_CATALOG_FROM_DB_REQUEST);
+
+      yield put(catalogActions.productCatalogRequestFetching(true));
+
+      const catalog = yield call(watchesService.getProductCatalogFromDB);
+
+      yield put(catalogActions.fillCatalog(catalog));
+    } catch ({ message }) {
+      yield put(catalogActions.hasProductCatalogFetchingError({ status: true, message }));
+    } finally {
+      yield put(catalogActions.productCatalogRequestFetching(false));
+    }
+  }
+}
+
+export default function* watchCatalog() {
+  yield all([call(fetchCatalog)]);
+}
