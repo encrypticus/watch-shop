71a241849bb0c94ca12e36ef6492b9081e3d46c1
encrypticus <encrypticus@gmail.com>
2020-09-09 19:39:19 +0600

    Product-cart saga: заменил эффект take в методах добавления и удаления товара на эффект takeEvery, добавил две дополнительные функции-вотчеры.

--- src/redux/sagas/product-cart.js
+++ src/redux/sagas/product-cart.js
@@ -1,5 +1,5 @@
 import {
-  take, put, call, all, delay,
+  take, takeEvery, put, call, all, delay,
 } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import remoteDBService from '#services/remote-db-service';
@@ -27,70 +27,72 @@ function* fetchProductCart() {
   }
 }
 
-function* addProductToCart() {
+function* addProductToCart(action) {
   let product;
 
-  while (true) {
-    try {
-      const action = yield take(cartActions.ADD_PRODUCT_TO_CART_REQUEST);
-      product = action.payload;
+  try {
+    product = action.payload;
 
-      yield put(cartActions.updateProductCartRequestFetching({ isFetching: true, product }));
+    yield put(cartActions.updateProductCartRequestFetching({ isFetching: true, product }));
 
-      yield put(catalogActions.updateProductCartRequestFetching({ isFetching: true, product }));
+    yield put(catalogActions.updateProductCartRequestFetching({ isFetching: true, product }));
 
-      const productData = yield call(remoteDBService.addProductToCart, product);
+    const productData = yield call(remoteDBService.addProductToCart, product);
 
-      const inCart = yield call(remoteDBService.updateProductCatalog, product.index, productData.name, true);
+    const inCart = yield call(remoteDBService.updateProductCatalog, product.index, productData.name, true);
 
-      yield put(catalogActions.updateCatalog({ index: product.index, uniqueId: inCart.uniqueId, inCart: true }));
+    yield put(catalogActions.updateCatalog({ index: product.index, uniqueId: inCart.uniqueId, inCart: true }));
 
-      const products = yield call(remoteDBService.getProductCartFromDB);
+    const products = yield call(remoteDBService.getProductCartFromDB);
 
-      yield put(cartActions.fillProductCart(products));
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      yield put(cartActions.updateProductCartRequestFetching({ isFetching: false, product }));
-      yield put(catalogActions.updateProductCartRequestFetching({ isFetching: false, product }));
-    }
+    yield put(cartActions.fillProductCart(products));
+  } catch (error) {
+    toast.error(error.message);
+  } finally {
+    yield put(cartActions.updateProductCartRequestFetching({ isFetching: false, product }));
+    yield put(catalogActions.updateProductCartRequestFetching({ isFetching: false, product }));
   }
 }
 
-function* removeProductFromCart() {
+function* removeProductFromCart(action) {
   let product;
 
-  while (true) {
-    try {
-      const action = yield take(cartActions.REMOVE_PRODUCT_FROM_CART_REQUEST);
-      product = action.payload;
+  try {
+    product = action.payload;
 
-      yield put(cartActions.updateProductCartRequestFetching({ isFetching: true, product }));
+    yield put(cartActions.updateProductCartRequestFetching({ isFetching: true, product }));
 
-      yield put(catalogActions.updateProductCartRequestFetching({ isFetching: true, product }));
+    yield put(catalogActions.updateProductCartRequestFetching({ isFetching: true, product }));
 
-      yield call(remoteDBService.removeProductFromCart, product);
+    yield call(remoteDBService.removeProductFromCart, product);
 
-      yield call(remoteDBService.updateProductCatalog, product.index, '', false);
+    yield call(remoteDBService.updateProductCatalog, product.index, '', false);
 
-      yield put(catalogActions.updateCatalog({ index: product.index, uniqueId: '', inCart: false }));
+    yield put(catalogActions.updateCatalog({ index: product.index, uniqueId: '', inCart: false }));
 
-      const products = yield call(remoteDBService.getProductCartFromDB);
+    const products = yield call(remoteDBService.getProductCartFromDB);
 
-      yield put(cartActions.hideCard(product));
+    yield put(cartActions.hideCard(product));
 
-      yield delay(500);
+    yield delay(500);
 
-      yield put(cartActions.fillProductCart(products));
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      yield put(cartActions.updateProductCartRequestFetching({ isFetching: true, product }));
-      yield put(catalogActions.updateProductCartRequestFetching({ isFetching: false, product }));
-    }
+    yield put(cartActions.fillProductCart(products));
+  } catch (error) {
+    toast.error(error.message);
+  } finally {
+    yield put(cartActions.updateProductCartRequestFetching({ isFetching: true, product }));
+    yield put(catalogActions.updateProductCartRequestFetching({ isFetching: false, product }));
   }
+}
+
+function* watchAddProductToCart() {
+  yield takeEvery(cartActions.ADD_PRODUCT_TO_CART_REQUEST, addProductToCart);
+}
+
+function* watchRemoveProductFromCart() {
+  yield takeEvery(cartActions.REMOVE_PRODUCT_FROM_CART_REQUEST, removeProductFromCart);
 }
 
 export default function* watchProductCart() {
-  yield all([call(fetchProductCart), call(addProductToCart), call(removeProductFromCart)]);
+  yield all([call(fetchProductCart), call(watchAddProductToCart), call(watchRemoveProductFromCart)]);
 }
