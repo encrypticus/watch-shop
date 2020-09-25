b885c82b5b395e023e45a29a813fa06f62087c01
encrypticus <encrypticus@gmail.com>
2020-09-25 16:05:14 +0600

    Заменил BrowserRouter на HashRouter.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer, Flip } from 'react-toastify';
 import IndexPage from '#pages/index';
