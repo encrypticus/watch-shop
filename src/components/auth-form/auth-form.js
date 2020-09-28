f5f8c9ff4aa4e0d6e93c8106e75ab50113fba1a4
encrypticus <encrypticus@gmail.com>
2020-09-28 19:36:56 +0600

    Component AuthForm: мелкий рефакторинг.

--- src/components/auth-form/auth-form.js
+++ src/components/auth-form/auth-form.js
@@ -17,6 +17,8 @@ const AuthForm = ({ type = 'signUp' }) => {
     USER_DISABLED: 'Учётная запись отключена администратором',
     EMAIL_EXISTS: 'Адрес электронной почты уже используется другим аккаунтом',
     OPERATION_NOT_ALLOWED: 'Вход с паролем отключен для этого проекта',
+    'WEAK_PASSWORD : Password should be at least 6 characters': 'Слишком короткий пароль',
+    'TOO_MANY_ATTEMPTS_TRY_LATER : Too many unsuccessful login attempts. Please try again later.': 'Превышено число попыток входа, попробуйте позже',
   };
 
   useEffect(() => {
@@ -75,7 +77,7 @@ const AuthForm = ({ type = 'signUp' }) => {
         />
       </div>
       {isAuthError ? <p
-        className='auth-form__error-message'>{errorCodes[message] || 'Превышено число попыток входа, попробуйте позже'}</p> : null}
+        className='auth-form__error-message'>{errorCodes[message] || 'Ошибка сети'}</p> : null}
       <div>
         {
           type === 'signUp'
