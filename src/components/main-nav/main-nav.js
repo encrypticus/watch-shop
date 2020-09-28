7ecdb5e1cf5b179412505ed0576dde7510a38464
encrypticus <encrypticus@gmail.com>
2020-09-28 21:36:02 +0600

    Component MainNav: доработка.

--- src/components/main-nav/main-nav.js
+++ src/components/main-nav/main-nav.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Nav, Wrapper } from './styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { isMenuOpen } from '#act/main-nav';
+import {
+  Nav,
+  Wrapper,
+  NavList,
+  NavItem,
+  StyledLink,
+} from './styles';
 import Hamburger from '#comps/hamburger';
 
 const MainNav = () => {
   const isOpen = useSelector((state) => state.mainNavReducer.isOpen);
+  const { isUserSignedIn } = useSelector((state) => state.authReducer);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(isMenuOpen(false));
+  };
+
+  const carts = (
+    <>
+      <NavItem>
+        <StyledLink to='/product-cart' onClick={handleClick}>Корзина</StyledLink>
+      </NavItem>
+      <NavItem>
+        <StyledLink to='/favorites-cart' onClick={handleClick}>Избранное</StyledLink>
+      </NavItem>
+    </>
+  );
 
   return (
     <Nav opened={isOpen}>
+      <NavList>
+        <NavItem>
+          <StyledLink to='/' onClick={handleClick}>Главная</StyledLink>
+        </NavItem>
+        <NavItem>
+          <StyledLink to='/catalog' onClick={handleClick}>Каталог</StyledLink>
+        </NavItem>
+        {isUserSignedIn && carts}
+      </NavList>
       <Wrapper>
         <Hamburger color='white'/>
       </Wrapper>
--- src/components/main-nav/styles.js
+++ src/components/main-nav/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Nav = styled.div`
   box-sizing: border-box;
@@ -11,6 +12,7 @@ export const Nav = styled.div`
   min-height: 100vh;
   height: 100%;
   padding: 10px;
+  padding-left: 30px;
   background-color: rgba(0, 0, 0, .9);
   transform: ${(props) => (props.opened ? 'translateX(0)' : 'translateX(-30vw)')};
   z-index: 10;
@@ -22,6 +24,7 @@ export const Nav = styled.div`
   }
   
   @media(max-width: 480px) {
+    padding-top: 30px;
     min-width: 100vw;
     transform: ${(props) => (props.opened ? 'translateX(0)' : 'translateX(-100vw)')};
   }
@@ -29,4 +32,33 @@ export const Nav = styled.div`
 
 export const Wrapper = styled.div`
   margin-left: auto;
+`;
+
+export const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  flex-grow: 1;
+`;
+
+export const NavItem = styled.li`
+
+`;
+
+export const StyledLink = styled(Link)`
+  display: block;
+  padding: 10px;
+  color: #fff;
+  text-decoration: none;
+  font-family: Roboto, Arial, sans-serif;
+  letter-spacing: 1px;
+  transition: background-color .3s;
+  
+  :hover {
+    background-color: #383838;
+  }
+  
+  @media(max-width: 480px) {
+    font-size: 20px;
+  }
 `;
